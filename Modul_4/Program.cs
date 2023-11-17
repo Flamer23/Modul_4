4b15df89fa7f60b1b38a70f04a0122895c52af37
Илья Зуев <zueviliya777@gmail.com>
2023-11-17 11:07:38 +0300

    Задание 4.1.5

--- Modul_4/Program.cs
+++ Modul_4/Program.cs
@@ -8,6 +8,13 @@
             string A = "abc" ;
             string B = "123";
             bool C = A != B;
+
+            //Задание 4.1.5
+            int AA = 0;
+            int BB = 1;
+            double X = 0.0;
+            double Y = 1.0;
+            bool CC = (AA < BB) | (X > Y);
         }
     }
 }
