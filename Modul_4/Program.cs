a142a02ccd85cd03feddfc8eca5d7bb48a9914cb
Илья Зуев <zueviliya777@gmail.com>
2023-11-17 11:21:02 +0300

    Задание 4.1.18

--- Modul_4/Program.cs
+++ Modul_4/Program.cs
@@ -62,6 +62,35 @@
                 Console.WriteLine("Your color is cyan!");
             }
 
+            //Задание 4.1.18
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
         }
     }
     
