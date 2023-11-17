00f8df2501fa15fa44a89f5f3b48e1f0a4d534fd
Илья Зуев <zueviliya777@gmail.com>
2023-11-17 11:54:28 +0300

    Задание 4.3.11

--- Modul_4/Program.cs
+++ Modul_4/Program.cs
@@ -140,6 +140,17 @@
                 Console.Write(name[i]);
             }
 
+
+            //Задание 4.3.11
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                    Console.Write(array[i, k] + " ");
+
+                Console.WriteLine();
+            }
         }
     }
     
