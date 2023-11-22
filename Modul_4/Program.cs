aa43c4e79476ba3dfe21cb5a767117c3c71b7e90
Ilya Zuev <Zueviliya777@gmail.com>
2023-11-22 14:03:24 +0300

    Задание 4.4.2

--- Modul_4/Program.cs
+++ Modul_4/Program.cs
@@ -242,6 +242,17 @@
                     }
                 }
             }
+
+            //Задание 4.4.2
+            (string name, int age) anketa;
+
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
         }
     }
     
